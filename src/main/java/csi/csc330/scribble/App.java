47824b0b1c63da790443b25d62b840ad52116aac
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-12-04 01:08:26 -0500

    re-added chalk icon

--- src/main/java/csi/csc330/scribble/App.java
+++ src/main/java/csi/csc330/scribble/App.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
+import javafx.scene.image.Image;
 
 public class App extends Application {
 
@@ -14,6 +17,11 @@ public class App extends Application {
         primaryStage.setTitle("Scribble");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+        Image chalkCursorImage = new Image(getClass().getResourceAsStream("/chalk.png"));
+        Cursor chalkCursor = new ImageCursor(chalkCursorImage);
+        primaryStage.getScene().setCursor(chalkCursor);
+        Image icon = new Image(getClass().getResourceAsStream("/icon.png"));
+        primaryStage.getIcons().add(icon);
     }
 
     public static void main(String[] args) {
